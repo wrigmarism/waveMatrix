13cd202d4b5a7fa948ecb25c6fdc546a0f6a7a9b
Christoffer Wrigmar <christoffer.wrigmar@knowit.se>
2021-11-03 14:23:25 +0100

    v1

---
+++ js/functions.js
@@ -0,0 +1,32 @@
+function createMatrix(size){
+    var matrix = []
+    for (i = 0; i < size; i++) {
+        var row = []
+        for (j = 0; j < (size*2); j++) {
+            row[j] = [(j+i), "■"]
+        }
+        matrix[i] = row        
+    }
+    return matrix
+}
+
+function clickEvent(count, reset){
+    if(reset){
+        for (let i = 0; i < count; i++){
+            setTimeout(function(){changeColor(i, "yellow"); }, (i*50))  
+        }
+    }
+    else{
+        for (let i = 0; i < count; i++){
+            setTimeout(function(){changeColor(i, "black"); }, (i*50))   
+        }
+    }
+}
+
+function changeColor(count, color){
+    cElements = document.getElementsByClassName('c' + count)
+        for (let i = 0; i < cElements.length; i++) {
+            document.getElementById(cElements[i].id).style.color=color
+            
+        }
+}
